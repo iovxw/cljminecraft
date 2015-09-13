8ad62b1399b633913a80e53363bfa38804d8f8d4
kolen <incredible.angst@gmail.com>
2015-09-14 02:33:57 +0300

    Use Spigot Maven repositories (and version of bukkit API from Spigot), also added Spigot API dependency

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :dependencies [[org.clojure/clojure         "1.5.1"]
                  [org.clojure/tools.logging   "0.2.6"]
                  [org.clojure/tools.nrepl     "0.2.3"]
-                 [org.bukkit/bukkit           "1.6.4-R2.0"]
                  [clojure-complete            "0.2.3"]
                  [cheshire                    "5.2.0"]
                  [org.reflections/reflections "0.9.8"]]
+                 [org.bukkit/bukkit           "1.8.8-R0.1-SNAPSHOT"]
+                 [org.spigotmc/spigot-api     "1.8.8-R0.1-SNAPSHOT"]
   :profiles {:dev {:dependencies []}}
   :javac-options [ "-d" "classes/" "-source" "1.6" "-target" "1.6"]
   :java-source-paths ["javasrc"]
-  :uberjar-exclusions [#"(org|com|gnu)[/](bukkit|avaje|yaml|getspout|json|trove)[/](.*)"
+  :uberjar-exclusions [#"(org|com|gnu)[/](bukkit|avaje|yaml|getspout|json|trove|spigotmc)[/](.*)"
                        #"com[/]google[/]common[/](.*)"
                        #"org[/]apache[/]commons[/](.*)"
                        #"javax[/]persistence[/](.*)"
                        #"net[/]sf[/]cglib[/](.*)"]
-  :repositories [["bukkit.snapshots" "http://repo.bukkit.org/content/repositories/snapshots"]
-                 ["bukkit.release"   "http://repo.bukkit.org/content/repositories/releases"]]
+  :repositories [["spigot.snapshots" "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"]]
   )
