c753be770019f5503c9edd3ddab0fc0fce3a0468
iovxw <iovxw@outlook.com>
2016-08-07 17:48:19 +0800

    fix bukkit dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject cljminecraft "1.0.0"
   :description "Clojure for Bukkit Minecraft"
   :repositories [["spigot.snapshots" "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.bukkit/bukkit "1.9-R0.1-SNAPSHOT"]]
+  :profiles {:provided
+             {:dependencies
+              [[org.bukkit/bukkit "1.9-R0.1-SNAPSHOT"]]}}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :java-source-paths ["javasrc"])
